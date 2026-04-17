241684586e997a0b4fb4905dfdcb1a4aaf6a4825
Noc-00 <pao.alonso902@gmail.com>
2026-04-17 16:22:34 -0600

    Actualizar CORS para aceptar Vercel

--- server/src/main/java/com/vinus/server/controllers/AuthController.java
+++ server/src/main/java/com/vinus/server/controllers/AuthController.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:5173", "https://vinus-app.vercel.app"}, allowCredentials = "true")
 public class AuthController {
 
     @Autowired
